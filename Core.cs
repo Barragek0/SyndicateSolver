3a9d8765d545a470b696cb6767f66c91b00fdc3b
Barragek0 <barragek0lol@gmail.com>
2021-10-31 10:55:50 +0000

    Add null checks to prevent deadlock

--- Core.cs
+++ Core.cs
@@ -50,17 +50,23 @@ namespace SyndicateSolver
                 for (var i = 0; i < betrayalWindow.ChildCount; i++)
                 {
                     var child = betrayalWindow.GetChildAtIndex(i);
-                    if (child.Children.Count > 0)
+                    if (child != null)
                     {
-                        for (var i1 = 0; i1 < child.ChildCount; i1++)
+                        if (child.Children.Count > 0)
                         {
-                            var childOfChild = betrayalWindow.GetChildAtIndex(i).GetChildAtIndex(i1);
-                            if (childOfChild.Text.Length > 0)
-                                LogMessage("BetrayalWindow child of child -> " + childOfChild.Text);
+                            for (var i1 = 0; i1 < child.ChildCount; i1++)
+                            {
+                                var childOfChild = betrayalWindow.GetChildAtIndex(i).GetChildAtIndex(i1);
+                                if (childOfChild != null)
+                                {
+                                    if (childOfChild.Text.Length > 0)
+                                        LogMessage("BetrayalWindow child of child -> " + childOfChild.Text == null ? "null" : childOfChild.Text);
+                                }
+                            }
                         }
+                        if (child.Text.Length > 0)
+                            LogMessage("BetrayalWindow child -> " + child.Text);
                     }
-                    if (child.Text.Length > 0)
-                        LogMessage("BetrayalWindow child -> " + child.Text);
                 }
 
                 var syndicatePanel = GameController.Game.IngameState.IngameUi.SyndicatePanel;
@@ -68,17 +74,23 @@ namespace SyndicateSolver
                 for (var i = 0; i < syndicatePanel.ChildCount; i++)
                 {
                     var child = syndicatePanel.GetChildAtIndex(i);
-                    if (child.Children.Count > 0)
+                    if (child != null)
                     {
-                        for (var i1 = 0; i1 < child.ChildCount; i1++)
+                        if (child.Children.Count > 0)
                         {
-                            var childOfChild = syndicatePanel.GetChildAtIndex(i).GetChildAtIndex(i1);
-                            if (childOfChild.Text.Length > 0)
-                                LogMessage("SyndicatePanel child of child -> " + childOfChild.Text);
+                            for (var i1 = 0; i1 < child.ChildCount; i1++)
+                            {
+                                var childOfChild = syndicatePanel.GetChildAtIndex(i).GetChildAtIndex(i1);
+                                if (childOfChild != null)
+                                {
+                                    if (childOfChild.Text.Length > 0)
+                                        LogMessage("SyndicatePanel child of child -> " + childOfChild.Text == null ? "null" : childOfChild.Text);
+                                }
+                            }
                         }
+                        if (child.Text.Length > 0)
+                            LogMessage("SyndicatePanel child -> " + child.Text);
                     }
-                    if (child.Text.Length > 0)
-                        LogMessage("SyndicatePanel child -> " + child.Text);
                 }
 
                 var syndicateTree = GameController.Game.IngameState.IngameUi.SyndicateTree;
@@ -86,17 +98,23 @@ namespace SyndicateSolver
                 for (var i = 0; i < syndicateTree.ChildCount; i++)
                 {
                     var child = syndicateTree.GetChildAtIndex(i);
-                    if (child.Children.Count > 0)
+                    if (child != null)
                     {
-                        for (var i1 = 0; i1 < child.ChildCount; i1++)
+                        if (child.Children.Count > 0)
                         {
-                            var childOfChild = syndicateTree.GetChildAtIndex(i).GetChildAtIndex(i1);
-                            if (childOfChild.Text.Length > 0)
-                                LogMessage("SyndicateTree child of child -> " + childOfChild.Text);
+                            for (var i1 = 0; i1 < child.ChildCount; i1++)
+                            {
+                                var childOfChild = syndicateTree.GetChildAtIndex(i).GetChildAtIndex(i1);
+                                if (childOfChild != null)
+                                {
+                                    if (childOfChild.Text.Length > 0)
+                                        LogMessage("SyndicateTree child of child -> " + childOfChild.Text == null ? "null" : childOfChild.Text);
+                                }
+                            }
                         }
+                        if (child.Text.Length > 0)
+                            LogMessage("SyndicateTree child -> " + child.Text);
                     }
-                    if (child.Text.Length > 0)
-                        LogMessage("SyndicateTree child -> " + child.Text);
                 }
             } catch (Exception e)
             {

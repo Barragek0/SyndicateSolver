0a3591ea71c7d29c539715dfc5faa1747589818e
Barragek0 <barragek0lol@gmail.com>
2021-10-31 10:11:10 +0000

    Initial commit

---
+++ Core.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ExileCore;
+using ExileCore.PoEMemory;
+using ExileCore.PoEMemory.Elements;
+using ExileCore.Shared.Enums;
+using SharpDX;
+
+namespace SyndicateSolver
+{
+    public class Core : BaseSettingsPlugin<Settings>
+    {
+        private bool _wasOpened;
+
+
+        public override void Render()
+        {
+            var betrayalWindow = GameController.Game.IngameState.IngameUi.BetrayalWindow;
+
+            if (betrayalWindow.IsVisibleLocal)
+            {
+                if (!_wasOpened)
+                {
+                    _wasOpened = true;
+                    ParseMembers(betrayalWindow);
+                    //    FindAndSolveCurrentInteraction(betrayalWindow);
+                }
+
+                // DrawSolvedData(betrayalWindow);
+            }
+            else
+            {
+                _wasOpened = false;
+            }
+        }
+
+        private void ParseMembers(Element betrayalWindow)
+        {
+            for (var i = 0; i < betrayalWindow.ChildCount; i++)
+            {
+                var child = betrayalWindow.GetChildAtIndex(i);
+                if (child.Children.Count > 0)
+                {
+                    for (var i1 = 0; i1 < child.ChildCount; i1++)
+                    {
+                        var childOfChild = betrayalWindow.GetChildAtIndex(i).GetChildAtIndex(i1);
+                        LogMessage("Found child of child -> " + childOfChild.Text);
+                    }
+                } else
+                {
+                    LogMessage("Found child -> "+child.Text);
+                }
+            }
+        }
+    }
+}
